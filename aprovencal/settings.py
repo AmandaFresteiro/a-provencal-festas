28407533a8187e208d19012585f7eff86ea0efbb
Amanda <amandagoncalveseng@gmail.com>
2020-12-01 13:58:20 -0300

    configura ALLOWED_HOSTS

--- aprovencal/settings.py
+++ aprovencal/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
